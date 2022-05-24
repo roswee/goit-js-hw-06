da04bf0a78128ccaf856d731f23e89efb1f936a2
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-05-24 14:17:26 +0200

    changed task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,12 @@
 const span = document.querySelector("#name-output");
 const inputValue = document.querySelector("#name-input");
 
-if (inputValue != null) {
-    inputValue.addEventListener("input", (event) => {
-        span.textContent = event.currentTarget.value;
-      });
-}
+
+const eHandler = () => {
+  if (inputValue.value === "") {
+    span.textContent = `Anonymous`
+  }
+else {
+      span.textContent = event.currentTarget.value;
+    }};
+inputValue.addEventListener("input", eHandler)
